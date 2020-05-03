93fa99161a1b1921590d9ed0c28eb15e025eb0c5
Max Mignolet <maxmignolet278@gmail.com>
2020-05-03 15:27:49 +0200

    minor change

--- Q1_1_1.m
+++ Q1_1_1.m
@@ -1,27 +1,27 @@
-function [p1, p2, Q_t] = Q1_1_1(time)
+function [p1, p2, Q_t] = Q1_1_1(nb_iterations)
 
 Q = [0 0.1 0.1 0.8 ; 1 0 0 0 ; 0.6 0 0.1 0.3 ; 0.4 0.1 0.5 0];
 
-p1 = zeros(4, time);
+p1 = zeros(4, nb_iterations);
 
 p1(:, 1) = 0.25;
 
-for i=2:time
+for i=2:nb_iterations
     p1(:, i) = transpose(Q) * p1(:, i-1);
 end
 
-p2 = zeros(4, time);
+p2 = zeros(4, nb_iterations);
 p2(3, 1) = 1;
 
-for i=2:time
+for i=2:nb_iterations
     p2(:, i) = transpose(Q) * p2(:, i-1);
 end
 
 s = size(Q);
-Q_t = zeros(s(1), s(2), time);
+Q_t = zeros(s(1), s(2), nb_iterations);
 Q_t(:, :, 1) = Q;
 
-for i=2:time
+for i=2:nb_iterations
     Q_t(:, :, i) = Q * Q_t(:, :, i-1);
 end
 
@@ -33,7 +33,7 @@ title({'Evolution de la distribution de probabilite', 'pour une distribution ini
 xlabel('Position t dans la chaine de Markov');
 ylabel('Probabilite');
 hold on;
-t = 1:time;
+t = 1:nb_iterations;
 for i=1:4
     plot(t, p1(i,:));
 end
@@ -45,7 +45,7 @@ title({'Evolution de la distribution de probabilite', 'avec comme etat initial l
 xlabel('Position t dans la chaine de Markov');
 ylabel('Probabilite');
 hold on;
-t = 1:time;
+t = 1:nb_iterations;
 for i=1:4
     plot(t, p2(i,:));
 end
