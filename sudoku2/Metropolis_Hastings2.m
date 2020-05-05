d62d1e67eb2be489a987c54b131ade6adb1d4be5
Max Mignolet <maxmignolet278@gmail.com>
2020-05-05 11:15:24 +0200

    cleaning

--- sudoku2/Metropolis_Hastings2.m
+++ sudoku2/Metropolis_Hastings2.m
@@ -1,5 +1,4 @@
-function [good_grid, nb_iteration, grids, f] = Metropolis_Hastings2(g)
-tic;
+function [good_grid, nb_iteration, grids, f] = Metropolis_Hastings2()
 max_chain_length = 10^6;
 beta = -log(0.05); % to be tuned
 
@@ -17,29 +16,29 @@ beta = -log(0.05); % to be tuned
 % 	[0 0 0 7 8 6 6 0 0];
 % 	]; % hard grid
 
-initial_grid = [
-[0 0 6 1 0 0 0 0 0];
-[0 1 3 2 4 0 0 6 7];
-[0 0 8 0 5 0 0 1 4];
-[0 5 2 0 0 7 0 0 8];
-[1 0 0 0 2 0 0 0 5];
-[7 0 0 9 0 0 2 3 0];
-[2 9 0 0 7 0 4 0 0];
-[3 4 0 0 9 8 1 2 0];
-[0 0 0 0 0 2 7 0 0];
-	]; % medium grid
-
 % initial_grid = [
-% 	[4 0 6 3 8 0 0 2 0];
-% 	[5 0 3 7 0 4 0 0 0];
-% 	[0 0 0 9 0 0 8 4 3];
-% 	[2 3 0 0 1 0 9 0 0];
-% 	[0 4 0 0 0 0 5 7 1];
-% 	[0 5 0 6 4 7 0 0 0];
-% 	[9 0 1 4 0 8 3 0 0];
-% 	[0 6 4 0 0 0 0 0 7];
-% 	[8 0 5 1 0 3 0 9 2];
-% 	]; % easy grid
+% [0 0 6 1 0 0 0 0 0];
+% [0 1 3 2 4 0 0 6 7];
+% [0 0 8 0 5 0 0 1 4];
+% [0 5 2 0 0 7 0 0 8];
+% [1 0 0 0 2 0 0 0 5];
+% [7 0 0 9 0 0 2 3 0];
+% [2 9 0 0 7 0 4 0 0];
+% [3 4 0 0 9 8 1 2 0];
+% [0 0 0 0 0 2 7 0 0];
+% 	]; % medium grid
+
+initial_grid = [
+	[4 0 6 3 8 0 0 2 0];
+	[5 0 3 7 0 4 0 0 0];
+	[0 0 0 9 0 0 8 4 3];
+	[2 3 0 0 1 0 9 0 0];
+	[0 4 0 0 0 0 5 7 1];
+	[0 5 0 6 4 7 0 0 0];
+	[9 0 1 4 0 8 3 0 0];
+	[0 6 4 0 0 0 0 0 7];
+	[8 0 5 1 0 3 0 9 2];
+	]; % easy grid
 
 
 % Compute the number of element missing for each value.
